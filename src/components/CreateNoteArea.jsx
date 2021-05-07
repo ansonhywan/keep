94607c7bb0b40340630f3960dd1bddbc2767e8bc
Anson Wan <anson.hy.wan@gmail.com>
2021-05-07 14:11:14 -0400

    Updated styling to use Material UI elements. Added logic to add note button.

--- src/components/CreateNoteArea.jsx
+++ src/components/CreateNoteArea.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateNoteArea(props) {
+  const [isExpanded, setExpanded] = useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: "",
@@ -27,21 +32,28 @@ function CreateNoteArea(props) {
 
   return (
     <div>
-      <form>
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
         <textarea
           name="content"
+          onClick={() => setExpanded(true)}
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
         />
-        <button onClick={submitNewNote}>+</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNewNote} disabled={!note.title && !note.content}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
